41e40988feee835a0dd89233f2055265e1faa2eb
Quinuel Tabot  N-A <97244109+QUIN04@users.noreply.github.com>
2023-01-12 11:16:42 +0100

    Add files via upload

---
+++ H-Task0.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main(){
+	//Program to sum numbers from 1 to n.
+	/* Algorithm:
+	- Get n
+	- Identify numbers less than n right down to  1
+	- Sum up the number and retrieve the total.*/
+	int i,n,sum;
+	printf("Enter a value:%d",n);// Getting the value of n from the user
+	scanf("&n");
+	for (i=1;i<=n;i++)
+	{
+		printf("%d \t",i);
+		scanf("&i");
+}
+sum = n+i ;
+printf("Sum of numbers = %d \n",sum);
+
+	return 0;
+}
+
+
---
+++ H-Task1.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main(){
+	//Program to count the number of alphabets.
+	/* Algorithm:
+	- Using 65 which is turned to binary num, A can be generated
+	- Identify the different letters  1
+	- Sum up the number of letters*/
+	char q,letters;
+	int n; //where n is the number of letters
+
+
+	for(q=65;q<=91;q++)
+	{
+		printf("%c \t",q);
+		scanf("&q");
+}
+n = 91 -65;
+printf("Number of letters = %d \n",n);
+
+	return 0;
+}
